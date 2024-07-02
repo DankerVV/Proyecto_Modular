f36253936e8b96b3ffdaf265204998215474936c
Carlos Salcido <cusalcidoa@gmail.com>
2024-07-02 12:06:15 -0600

    Boton Basico a medias

--- lib/presentation/views/inicio_views.dart
+++ lib/presentation/views/inicio_views.dart
@@ -5,13 +5,40 @@ class InicioView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
+    return  Center(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Text('Página de Inicio'),
+          basicButton()
         ],
       ),
     );
   }
+}
+
+Widget basicButton (){
+  return(
+    ElevatedButton(
+      onPressed: (){
+        //TO DO: Al presionar se activa el NFC para pagar
+      },
+      style: ElevatedButton.styleFrom(
+        padding: const EdgeInsets.symmetric(vertical: 15.0, horizontal: 120.0),
+      ),
+      child: Row(
+        //mainAxisSize: MainAxisSize.min,
+        //mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          const Expanded(child: Text('Básico', textAlign: TextAlign.left,),),
+          //const Text('Básico'),
+          IconButton(
+            icon: const Icon(Icons.more_vert),
+            onPressed: () {
+              //TO DO: Al presionar se abre el menu de opciones
+            },
+         ),
+        ],
+      ),
+    )
+  );
 }
