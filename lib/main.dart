ee0cb629294f5c0d1edc3876a6d8aa9fdfe2d3e3
Carlos Salcido <cusalcidoa@gmail.com>
2024-07-04 00:00:22 -0600

    Interfaz de Configuracion solo con modo oscuro funcional

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:proyecto_modular/config/theme/app_theme.dart';
+import 'package:proyecto_modular/config/theme/theme_provider.dart';
 import 'package:proyecto_modular/presentation/screens/main_screen.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    const ProviderScope(child: MyApp(),)
+  );
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends HookConsumerWidget {
   const MyApp({super.key});
 
   // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final appThemeState = ref.watch(appThemeStateNotifier);
     return MaterialApp(
       title: 'Flutter Modular',
       debugShowCheckedModeBanner: false,
-      theme: AppTheme(selectedColor: 0).theme(),
+      //theme: AppTheme(selectedColor: 0).theme(), 
+      theme: AppTheme.lightTheme,
+      darkTheme: AppTheme.darkTheme,
+      themeMode: appThemeState.isDarkModeEnabled ? ThemeMode.dark : ThemeMode.light,
       home: const MainScreen(),
     );
   }
--- lib/presentation/views/settings_view.dart
+++ lib/presentation/views/settings_view.dart
@@ -1,17 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:proyecto_modular/config/theme/theme_provider.dart';
 
-class SettingsView extends StatelessWidget {
+class SettingsView extends HookConsumerWidget {
   const SettingsView({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const Center(
+  Widget build(BuildContext context, WidgetRef ref) {
+    return  SafeArea(
       child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.start,
         children: [
-          Text('Página de Configuración'),
+          const SizedBox(height: 50,),
+          darkMode(ref),
         ],
       ),
     );
   }
+}
+
+Widget darkMode (WidgetRef ref){
+  final appThemeState = ref.watch(appThemeStateNotifier);
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.start,
+    children: [
+      const SizedBox(width: 20),
+      const Text('Modo Oscuro', style: TextStyle(fontSize: 20),),
+      const SizedBox(width: 10),
+      Switch(
+    value: appThemeState.isDarkModeEnabled,
+    onChanged: (enabled) {
+      if (enabled) {
+        appThemeState.setDarkTheme();
+      } else {
+        appThemeState.setLightTheme();
+      }
+    },
+  ),
+    ],
+  );
 }
