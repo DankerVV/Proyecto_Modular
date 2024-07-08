175850c72c39c6879d1a40aee5a39cc08fef7144
Carlos Salcido <cusalcidoa@gmail.com>
2024-07-08 16:17:48 -0600

    Agregar folder inicio_views

---
+++ lib/presentation/views/inicio_views/reload_physical_card_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ReloadPhysicalCard extends StatelessWidget {
+  const ReloadPhysicalCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
