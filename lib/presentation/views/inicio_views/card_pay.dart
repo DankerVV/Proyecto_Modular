e5cc1892c6a525775c3e0d098415502d0bb4ab74
Carlos Salcido <cusalcidoa@gmail.com>
2024-07-10 19:28:53 -0600

    Primer intento de NFC: fallido

--- lib/presentation/views/inicio_views/card_pay.dart
+++ lib/presentation/views/inicio_views/card_pay.dart
@@ -1,7 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:nfc_manager/nfc_manager.dart';
 
-class CardPay extends StatelessWidget {
+class CardPay extends StatefulWidget {
   const CardPay({super.key});
+
+  @override
+  State<CardPay> createState() => _CardPayState();
+}
+
+class _CardPayState extends State<CardPay> {
+  @override
+  void initState() {
+    super.initState();
+    startNFCReading();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,5 +30,28 @@ class CardPay extends StatelessWidget {
         ),
       ),
     );
+  }
+}
+
+void startNFCReading() async {
+  try {
+    bool isAvailable = await NfcManager.instance.isAvailable();
+    //We first check if NFC is available on the device.
+    if (isAvailable) {
+    //If NFC is available, start an NFC session and listen for NFC tags to be discovered.
+    NfcManager.instance.startSession(
+      onDiscovered: (NfcTag tag) async {
+        // Process NFC tag, When an NFC tag is discovered, print its data to the console.
+        debugPrint('NFC Tag Detected: ${tag.data}');
+        print('NFC Tag Detected: ${tag.data}');
+      },
+    );
+    } else {
+      debugPrint('NFC not available.');
+      print('NFC not available.');
+    }
+  } catch (e) {
+    debugPrint('Error reading NFC: $e');
+    print('Error reading NFC: $e');
   }
 }
