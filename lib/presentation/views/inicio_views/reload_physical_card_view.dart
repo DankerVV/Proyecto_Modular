87afd1c2ae1b3cd09e40284f4853cf7be8fdaad2
Carlos Salcido <cusalcidoa@gmail.com>
2024-07-17 10:48:58 -0600

    Segundo intento de detectar NFC: Hace falta probar en un android fisico

--- lib/presentation/views/inicio_views/reload_physical_card_view.dart
+++ lib/presentation/views/inicio_views/reload_physical_card_view.dart
@@ -1,7 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:nfc_manager/nfc_manager.dart';
 
-class ReloadPhysicalCard extends StatelessWidget {
+class ReloadPhysicalCard extends StatefulWidget {
   const ReloadPhysicalCard({super.key});
+
+  @override
+  State<ReloadPhysicalCard> createState() => _ReloadPhysicalCardState();
+}
+
+class _ReloadPhysicalCardState extends State<ReloadPhysicalCard> {
+  String _nfcMessage = 'Mantén tu tarjeta MiPasaje cerca del celular';
+
+  @override
+  void initState() {
+    super.initState();
+    startNFCReading();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -9,15 +23,55 @@ class ReloadPhysicalCard extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Recargar una tarjeta Física"),
       ),
-      body: const Center(
+      body: Center(
         child: Text(
-          'Mantén tu tarjeta MiPasaje cerca del celular',
-          style: TextStyle(fontSize: 20),
+          _nfcMessage,
+          style: const TextStyle(fontSize: 20),
           textAlign: TextAlign.center,
         ),
       ),
     );
   }
-}
 
-void nfcReader(BuildContext context){}
+  void startNFCReading() async {
+    try {
+      bool isAvailable = await NfcManager.instance.isAvailable();
+      debugPrint('NFC availability checked: $isAvailable');
+      // Comprobar si NFC está disponible en el dispositivo
+      if (isAvailable) {
+        setState(() {
+          _nfcMessage = 'Esperando una tarjeta NFC...';
+        });
+        // Iniciar la sesión NFC y escuchar por etiquetas NFC descubiertas
+        NfcManager.instance.startSession(
+          onDiscovered: (NfcTag tag) async {
+            // Procesar la etiqueta NFC
+            debugPrint('NFC Tag Detected: ${tag.data}');
+            setState(() {
+              _nfcMessage = 'Etiqueta NFC detectada: ${tag.data}';
+            });
+            // Detener la sesión NFC después de la detección
+            await NfcManager.instance.stopSession();
+          },
+          onError: (error) async {
+            debugPrint('NFC Session Error: $error');
+            setState(() {
+              _nfcMessage = 'Error en la sesión NFC: $error';
+            });
+            await NfcManager.instance.stopSession(errorMessage: error.toString());
+          },
+        );
+      } else {
+        setState(() {
+          _nfcMessage = 'NFC no está disponible en este dispositivo.';
+        });
+        debugPrint('NFC not available.');
+      }
+    } catch (e) {
+      setState(() {
+        _nfcMessage = 'Error leyendo NFC: $e';
+      });
+      debugPrint('Error reading NFC: $e');
+    }
+  }
+}
