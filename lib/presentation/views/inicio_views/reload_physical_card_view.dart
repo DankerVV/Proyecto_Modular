c7567f37adcd02e1a9f49bbff27cd1c1d537db6b
Carlos Salcido <cusalcidoa@gmail.com>
2024-09-09 13:44:13 -0600

    Funciona el pago y recarga de tarjeta, hay un problema de logica

--- lib/presentation/views/inicio_views/reload_physical_card_view.dart
+++ lib/presentation/views/inicio_views/reload_physical_card_view.dart
@@ -1,8 +1,8 @@
-import 'dart:convert';
 import 'dart:typed_data';
 import 'package:flutter/material.dart';
 import 'package:nfc_manager/nfc_manager.dart';
 import 'package:nfc_manager/platform_tags.dart';
+import 'package:proyecto_modular/config/pagos/procesar_pags.dart';
 
 class ReloadPhysicalCard extends StatefulWidget {
   const ReloadPhysicalCard({super.key});
@@ -12,7 +12,11 @@ class ReloadPhysicalCard extends StatefulWidget {
 }
 
 class _ReloadPhysicalCardState extends State<ReloadPhysicalCard> {
-  String _nfcMessage = 'Mantén tu tarjeta MiPasaje cerca del celular';
+  String _nfcMessage = 'Mantén tu tarjeta Mi Pasaje cerca del celular';
+  final _amountController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+  double _saldo = -1111.11;
+  final GlobalKey<ScaffoldMessengerState> _scaffoldMessengerKey = GlobalKey<ScaffoldMessengerState>();
 
   @override
   void initState() {
@@ -25,17 +29,63 @@ class _ReloadPhysicalCardState extends State<ReloadPhysicalCard> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Recargar una tarjeta Física"),
+        //title: const Text("Recarga una Tarjeta Física"),
       ),
-      body: Center(
-        child: Text(
-          _nfcMessage,
-          style: const TextStyle(fontSize: 20),
-          textAlign: TextAlign.center,
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child: 
+          Column(
+            children: [
+              const SizedBox(height: 75),
+              Text(
+                _nfcMessage,
+                style: const TextStyle(fontSize: 20),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 75,),
+              TextFormField(
+                decoration: const InputDecoration(labelText: 'Cantidad a añadir'),
+                controller: _amountController,
+                keyboardType: TextInputType.number,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Por favor, introduce una cantidad';
+                  }
+                  final amount = double.tryParse(value);
+                  if (amount == null || amount < 10) {
+                    return 'La cantidad debe ser al menos 10 MXN';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    if(_saldo != -1111.11){// verificacion para saber si se leyó la carta
+                      final amount = double.parse(_amountController.text);
+                      double saldoFinal = amount + _saldo;
+                      pagar(saldoFinal, amount);
+                    }
+                  }
+                },
+                child: const Text('Añadir Fondos'),
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
+
+  Future<void> pagar(double saldoFinal, double amount) async {
+  bool pago = await procesarPago(amount, _scaffoldMessengerKey);
+  if (pago) {
+    startNFCWriting(saldoFinal);
+  }
+}
   
   void startNFCReading() async {
     bool isAvailable = await NfcManager.instance.isAvailable();
@@ -55,28 +105,64 @@ class _ReloadPhysicalCardState extends State<ReloadPhysicalCard> {
             List<int> blockData = await mifareClassic.readBlock(blockIndex: 4);
             String data = String.fromCharCodes(blockData);
             String numericData = data.replaceAll(RegExp(r'[^0-9.]'), '');
-
-            setState(() {
-              _nfcMessage = 'Saldo actual: $numericData';
-            });
+            _saldo = double.parse(numericData);
+            changeMessage('Saldo actual: $numericData');
           } catch (e) {
-            setState(() {
-              _nfcMessage = 'Error al leer la tarjeta: $e';
-            });
+            changeMessage('Error al leer la tarjeta: $e');
           }
         }
         else {
-          setState(() {
-            _nfcMessage = 'No se pudo reconocer la tarjeta, debe ser tipo Mifare Classic.';
-          });
+          changeMessage('No se pudo reconocer la tarjeta, debe ser tipo Mifare Classic.');
         }
-        
         await NfcManager.instance.stopSession();
       });
     } else {
-      setState(() {
-        _nfcMessage = 'NFC no está disponible en este dispositivo.';
-      });
+      changeMessage('NFC no está disponible en este dispositivo.');
     }
+  }
+
+  void startNFCWriting (double saldoFinal) async{
+    bool isAvailable = await NfcManager.instance.isAvailable();
+    if (isAvailable) {
+      NfcManager.instance.startSession(onDiscovered: (NfcTag tag) async {
+        var mifareClassic = MifareClassic.from(tag);
+        if (mifareClassic != null) {
+          try {
+            // Convertir la clave a Uint8List
+            Uint8List key = Uint8List.fromList([0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF]);
+            
+            // Autenticar el sector 1 con la clave A
+            await mifareClassic.authenticateSectorWithKeyA(
+              sectorIndex: 1,
+              key: key,
+            );
+            
+            // Convertir el valor a escribir en una lista de bytes (max 16 bytes)
+            String valueString = saldoFinal.toString();
+            List<int> valueBytes = List<int>.from(valueString.codeUnits);
+            valueBytes += List<int>.filled(16 - valueBytes.length, 0); // Rellenar con ceros si es necesario
+
+            // Intentar escribir en el bloque 4, que está en el sector 1
+            Uint8List dataToWrite = Uint8List.fromList(valueBytes);
+            await mifareClassic.writeBlock(blockIndex: 4, data: dataToWrite);
+            changeMessage('Saldo actualizado: $saldoFinal');
+            
+          } catch (e) {
+            changeMessage('Error al escribir en la tarjeta: $e');
+          }
+        } else {
+          changeMessage('No se pudo reconocer la tarjeta, debe ser tipo Mifare Classic.');
+        }
+        await NfcManager.instance.stopSession();
+      });
+    } else {
+      changeMessage('NFC no está disponible en este dispositivo.');
+    }
+  }
+
+  void changeMessage(String frase){
+    setState(() {
+        _nfcMessage = frase;
+      });
   }
 }
