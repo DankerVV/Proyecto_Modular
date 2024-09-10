0c8a0de8f39de39f46a106a74473877bb9b349aa
Carlos Salcido <cusalcidoa@gmail.com>
2024-09-10 15:17:50 -0600

    Imposible detectar la terminal, se reemplaza con unaa tarjeta NFC con un valor especial

--- lib/presentation/views/inicio_views/card_pay.dart
+++ lib/presentation/views/inicio_views/card_pay.dart
@@ -1,81 +1,101 @@
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
 import 'package:nfc_manager/nfc_manager.dart';
+import 'package:nfc_manager/platform_tags.dart';
 
 class CardPay extends StatefulWidget {
   final String cardType;
-  const CardPay({super.key, required this.cardType});//recibir un String en la variable cardType
+  const CardPay({super.key, required this.cardType});
 
   @override
   State<CardPay> createState() => _CardPayState();
 }
 
 class _CardPayState extends State<CardPay> {
+  String nfcMessage = 'Acerca tu dispositivo a la terminal';
+
   @override
   void initState() {
     super.initState();
-    startNFCFinder(widget.cardType, context);
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      startNFCFinder(widget.cardType, context);
+    });
   }
 
-   @override
+  @override
   void dispose() {
-    // Detén la sesión NFC cuando se sale de la pantalla actual
     NfcManager.instance.stopSession();
     super.dispose();
   }
 
-
   @override
   Widget build(BuildContext context) {
-    //NOTA: USAR ESTE MISMO ARCHIVO PARA PAGAR CON CUALQUIER TARJETA, RECIBIENDO ALGÚN PARÁMETRO O ALGO
     return Scaffold(
       appBar: AppBar(
         title: const Text("Pago"),
       ),
-      body: const Center(
+      body: Center(
         child: Text(
-          'Acerca tu celular a la terminal de pago',
-          style: TextStyle(fontSize: 20),
+          nfcMessage,
+          style: const TextStyle(fontSize: 20),
           textAlign: TextAlign.center,
         ),
       ),
     );
   }
+
+  void startNFCFinder(String cardType, BuildContext context) async {
+    bool isAvailable = await NfcManager.instance.isAvailable();
+      if (isAvailable) {
+        NfcManager.instance.startSession(onDiscovered: (NfcTag tag) async {
+          var mifareClassic = MifareClassic.from(tag);
+          if (mifareClassic != null) {
+            try {
+              // Convertir la clave a Uint8List
+              Uint8List key = Uint8List.fromList([0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF]);
+              // Autenticar el sector 1 con la clave A
+              await mifareClassic.authenticateSectorWithKeyA(
+                sectorIndex: 1,
+                key: key,
+              );
+              // Intentar leer el bloque 4, que está en el sector 1
+              List<int> blockData = await mifareClassic.readBlock(blockIndex: 4);
+              String data = String.fromCharCodes(blockData);
+              String numericData = data.replaceAll(RegExp(r'[^0-9.]'), '');
+              double content = double.parse(numericData);
+              if(content == 1.234){// Este es el valor de la tarjeta que será la terminal
+                if (cardType == 'basico'){
+                  processPayment(9.50);
+                }
+                else if(cardType == 'verde'){
+                  pasajePayment();//siempre es un solo pago
+                }
+                else if(cardType == 'amarillo'){
+                  processPayment(4.75);
+                }
+                else if(cardType == 'supervisor'){
+                  pasajePayment();
+                }
+              }
+            } catch (e) {
+              changeMessage('Error al buscar la terminal: $e');
+            }
+          }
+          await NfcManager.instance.stopSession();
+        });
+      } else {
+        changeMessage('Esta función no está disponible en tu dispositivo.');
+      }
+  }
+
+  void changeMessage(String frase){
+    setState(() {
+        nfcMessage = frase;
+      });
+  }
 }
 
-void startNFCFinder(String cardType, BuildContext context) async{
-  try{
-    NfcManager.instance.startSession(
-      onDiscovered: (NfcTag tag) async {
-        print('Tag NFC detectado: $tag');
-        if (cardType == 'basico'){
-          processPayment(9.50);
-        }
-        else if(cardType == 'verde'){
-          pasajePayment();//siempre es un solo pago
-        }
-        else if(cardType == 'amarillo'){
-          processPayment(4.75);
-        }
-        else if(cardType == 'supervisor'){
-          //TODO: pagar nada, solo desbloquear
-        }
-
-        await NfcManager.instance.stopSession();
-      },
-      onError: (e){
-        // Maneja cualquier error que ocurra durante la sesión NFC
-        print('Error en la sesión NFC: $e');
-        NfcManager.instance.stopSession();
-        throw Exception('Error al manejar el NFC: $e');
-       },
-    );
-  }
-  catch(e){
-    print('Error general: $e');
-    // Detén la sesión NFC en caso de error
-    await NfcManager.instance.stopSession();
-  }
-}
 
 void processPayment (double precio){}
 
